284018c2507831c027cdb42800592b463282ee6d
Uladzislau Brel <vlad.brel.97@mail.ru>
2022-04-18 17:01:40 +0300

    Changed REST Controllers

--- src/main/java/by/brel/rest/api/v1/controller/BalanceRESTController.java
+++ src/main/java/by/brel/rest/api/v1/controller/BalanceRESTController.java
@@ -15,8 +15,8 @@ public class BalanceRESTController {
     private BalanceService balanceService;
 
     @GetMapping("/balances")
-    public List<Balance> getAllBalances(@RequestParam(defaultValue = "1") int page) {
-        return balanceService.getAllBalances(page);
+    public List<Balance> getAllBalances() {
+        return balanceService.getAllBalances();
     }
 
     @GetMapping("/balances/{id}")

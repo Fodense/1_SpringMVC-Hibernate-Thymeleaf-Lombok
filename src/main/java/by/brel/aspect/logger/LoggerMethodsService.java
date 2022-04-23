383128cb75509a84443ae378aba63e4f07fd3305
Uladzislau Brel <vlad.brel.97@mail.ru>
2022-04-23 18:14:05 +0300

    Created Logging with AOP

---
+++ src/main/java/by/brel/aspect/logger/LoggerMethodsService.java
@@ -0,0 +1,69 @@
+package by.brel.aspect.logger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerMethodsService {
+
+    @Around("@annotation(by.brel.aspect.LogExecutionTime)")
+    public Object startAndStopMethodsService(ProceedingJoinPoint joinPoint) {
+        final StopWatch watch = new StopWatch();
+
+        Object target = null;
+        try {
+            watch.start();
+            target = joinPoint.proceed();
+            watch.stop();
+
+        } catch (Throwable e) {
+            log.info(e.getMessage());
+        }
+
+        log.info(joinPoint.getSignature().getName() + " выполнен за " + watch.getTotalTimeMillis() + "мс");
+
+        return target;
+    }
+
+    @Around("by.brel.aspect.PointCuts.pointCutLoggerSave()")
+    public Object trySaveMethodsService(ProceedingJoinPoint joinPoint) {
+        Object target = null;
+
+        log.info("Попытка сохранения");
+
+        try {
+            target = joinPoint.proceed();
+
+        } catch (Throwable e) {
+            log.debug(e.getMessage());
+        }
+
+        log.info("Сохранено!");
+
+        return target;
+    }
+
+    @Around("by.brel.aspect.PointCuts.pointCutLoggerDelete()")
+    public Object tryDeleteMethodsService(ProceedingJoinPoint joinPoint) {
+        Object target = null;
+
+        log.info("Попытка удаления");
+
+        try {
+            target = joinPoint.proceed();
+
+        } catch (Throwable e) {
+            log.debug(e.getMessage());
+        }
+
+        log.info("Удалено!");
+
+        return target;
+    }
+}

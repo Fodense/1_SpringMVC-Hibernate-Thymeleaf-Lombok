ebe7ecf84286483072539329fc11793d808f39d8
Uladzislau Brel <vlad.brel.97@mail.ru>
2022-04-30 16:03:06 +0300

    Created new files xml for migration and changed index page

--- src/main/java/by/brel/config/WebSecurityConfig.java
+++ src/main/java/by/brel/config/WebSecurityConfig.java
@@ -1,27 +1,34 @@
 package by.brel.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("vlad").password("{noop}vlad").roles("MANAGER")
-                .and()
-                .withUser("ivan").password("{noop}ivan").roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasAnyRole("MANAGER", "USER")
+                .antMatchers("/").hasAnyRole("USER", "MANAGER")
                 .antMatchers("/customers/new").hasRole("MANAGER")
                 .antMatchers("/tariffs/new").hasRole("MANAGER")
                 .antMatchers("/balances/new").hasRole("MANAGER")

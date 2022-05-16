0abd3fc6b68395d5f2d4143c80c83975355c62cc
Uladzislau Brel <vlad.brel.97@mail.ru>
2022-05-16 17:43:30 +0300

    Fix bag with 403 Forbidden in POST requests

--- src/main/java/by/brel/config/WebSecurityConfig.java
+++ src/main/java/by/brel/config/WebSecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 @Configuration
@@ -29,7 +32,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+            private final AntPathRequestMatcher[] requestMatchers = {
+                    new AntPathRequestMatcher("/swagger-ui.html#/**")
+            };
+
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                for (AntPathRequestMatcher requestMatcher : requestMatchers) {
+                    if (requestMatcher.matches(request)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        };
+
+        http
+                .csrf()
+                .ignoringRequestMatchers(csrfRequestMatcher)
+                .and()
+                .authorizeRequests()
                 .antMatchers("/").hasAnyRole("USER", "MANAGER")
                 .antMatchers("/customers/new").hasRole("MANAGER")
                 .antMatchers("/tariffs/new").hasRole("MANAGER")
